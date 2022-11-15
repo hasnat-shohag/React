24952175751e3724f96a27ef4ba0653247befa6e
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-11-15 19:50:05 +0600

    class component

--- myreact/src/index.js
+++ myreact/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock({ local }) {
-    return (
-        <div>
-            This is a heading
-            <span> {new Date().toLocaleDateString(local)}</span>
-            <h1>hey</h1>
-            <p>whtsUp</p>
-        </div>
-    );
+class Clock extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                This is a heading
+                <span> {new Date().toLocaleDateString(this.props.local)}</span>
+                <h1>hey</h1>
+                <p>whtsUp</p>
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<Clock local="ur-IN" />, document.getElementById('root'));
+ReactDOM.render(<Clock local="bn-BD" />, document.getElementById('root'));
+
+// function Clock({ local }) {
+//     return (
+//         <div>
+//             This is a heading
+//             <span> {new Date().toLocaleDateString(local)}</span>
+//             <h1>hey</h1>
+//             <p>whtsUp</p>
+//         </div>
+//     );
+// }
