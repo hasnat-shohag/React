b840317da14f0568585525abd4041c33a4adb6d7
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-11-15 18:46:31 +0600

    functional component

--- myreact/src/index.js
+++ myreact/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let number = 5;
+function Clock({ local }) {
+    return (
+        <div>
+            This is a heading
+            <span> {new Date().toLocaleDateString(local)}</span>
+            <h1>hey</h1>
+            <p>whtsUp</p>
+        </div>
+    );
+}
 
-setInterval(() => {
-    const element = <h1>Hello Google {(number += 1)}</h1>;
-    ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+ReactDOM.render(<Clock local="ur-IN" />, document.getElementById('root'));
